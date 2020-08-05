c3e54f03af6f80941b85abbf3e82d9e83a3b4beb
Jeeson Domiguez <dominguez_js@outlook.com>
2020-08-05 13:12:53 -0600

    header and banner ready

--- js/tools.js
+++ js/tools.js
@@ -1,66 +1,65 @@
-$(window).on("load", function() {
-
-    $('.animated').each(function() {
-        var item = $(this)
-        var delay = $(this).data("delay")
+window.addEventListener("load", function(){
+    document.querySelectorAll('.animated').forEach(function(item){
+        var delay = item.getAttribute("data-delay");
 
         if (delay == null) {
             delay = 0;
         }
 
-        if ($(this).data("scroll") == 0 || $(this).data("scroll") == null) {
+        if (item.getAttribute("data-scroll") == 0 || item.getAttribute("data-scroll") == null){
             setTimeout(function() {
-                item.addClass('in');
+                item.classList.add('in');
             }, delay);
         }
-
     });
 });
 
-
-$(window).scroll(function() {
-    $('.animated').each(function() {
-        var position = $(this).offset().top;
-        var delay = $(this).data("delay")
-        var item = $(this)
+window.addEventListener("scroll", function(){
+    document.querySelectorAll('.animated').forEach(function(item){
+        var position = item.offsetTop;
+        var delay = item.getAttribute("data-delay");
         if (delay == null) {
             delay = 0;
         }
-        $.fn.scrollBottom = function() {
-            return $(document).height() - this.scrollTop() - this.height();
-        };
-        var topOfWindow = $(window).scrollTop();
-        var bottomOfWindow = $(window).scrollBottom();
 
-        if ($(this).data("scroll") != 0 || $(this).data("scroll") != null) {
+        function scrollBottom(){
+            return document.height - item.scrollTop - item.clientHeight;
+        }
+
+        var topOfWindow = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+        var bottomOfWindow = window.pageYOffset || document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+
+        if(item.getAttribute("data-scroll") != 0 && item.getAttribute("data-scroll") != null){
             if (position < topOfWindow + 700 || position == bottomOfWindow) {
                 setTimeout(function() {
-                    item.addClass('in');
+                    item.classList.add('in');
                 }, delay);
             }
         }
     });
 });
 
-$('.local-scroll').click(function(e) {
-    var jump = $(this).attr('href');
-    var position = $(jump).offset();
-    $('body, html').stop().animate({
-        scrollTop: position.top
-    }, 1000);
-    e.preventDefault();
-});
+// $(window).scroll(function() {
+//     $('.animated').each(function() {
+//         // var position = $(this).offset().top;
+//         // var delay = $(this).data("delay")
+//         // var item = $(this)
+//         // if (delay == null) {
+//         //     delay = 0;
+//         // }
+//         // $.fn.scrollBottom = function() {
+//         //     return $(document).height() - this.scrollTop() - this.height();
+//         // };
+//         // var topOfWindow = $(window).scrollTop();
+//         // var bottomOfWindow = $(window).scrollBottom();
 
-// $(window).load(function() {
-//   setTimeout(function() {
-//     $("#site-preloader").css({'opacity':'0'});
-//   }, 700);
-
-//   setTimeout(function() {
-//     $(document.body).css({'overflow':'auto'});
-//   }, 250);
-
-//   setTimeout(function() {
-//     $("#site-preloader").css({'display':'none'});
-//   }, 550);
+//         if ($(this).data("scroll") != 0 || $(this).data("scroll") != null) {
+//             if (position < topOfWindow + 700 || position == bottomOfWindow) {
+//                 setTimeout(function() {
+//                     item.addClass('in');
+//                 }, delay);
+//             }
+//         }
+//     });
 // });
